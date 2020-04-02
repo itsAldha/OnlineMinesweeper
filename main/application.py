4eb8dba99b9a2f3f833cffbbc635f6327f703f41
aldha <aldha@mail.com>
2020-04-02 18:28:26 -0400

    Implementing Cookies

--- main/application.py
+++ main/application.py
@@ -8,17 +8,27 @@ application = Flask(__name__)
 # Global Variables
 visitors = 0
 
+@application.route('/who')
+def who():
+    username = request.cookies.get('username')
+    return username
+
 @application.route('/login')
 def login():
     return 'login'
+
 
 @application.route('/user/<username>')
 def profile(username):
     return '{}\'s profile'.format(escape(username))
 
+@application.route('/')
 @application.route('/<val>')
 def index(val=None):
-    return render_template('index.html', name=val)
+    resp = make_response(render_template(...))
+    password = visitors+20
+    resp.set_cookie('username', password)
+    return render_template('index.html', name=val), resp
 
 @application.route('/counter')
 def counter():

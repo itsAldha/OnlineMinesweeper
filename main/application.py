10e5f38181c7204268c7bc5947573c30bcae25d4
aldha <aldha@mail.com>
2020-04-02 17:52:31 -0400

    Fix: Request Context

--- main/application.py
+++ main/application.py
@@ -16,7 +16,7 @@ def login():
 def profile(username):
     return '{}\'s profile'.format(escape(username))
 
-with app.test_request_context():
+with application.test_request_context():
     print(url_for('login'))
     print(url_for('login', next='/'))
     print(url_for('profile', username='John Doe'))
